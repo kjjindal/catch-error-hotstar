d29df37819d800c55e8332076799086ad65868f6
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-09-28 20:18:06 +0530

    catch error : hotstar clone using react js
    hotstar clone using react js and html and css and javascript and redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hotstar from './Hotstar';
+
+function App() {
+  return (
+    <div className="app">
+
+    <Hotstar  />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit';
+import optionReducer from '../features/optionSlice';
+
+
+export const store=configureStore({
+    reducer:{
+        option:optionReducer
+    }
+})
